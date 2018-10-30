705b7c0d8e63b23d650878a4fe541a1495c0c840
pleete <qsdr92@nate.com>
2018-10-30 17:19:08 +0900

    GpsService 추가

--- app/src/main/java/edu/android/examplemap3/MapsActivity.java
+++ app/src/main/java/edu/android/examplemap3/MapsActivity.java
@@ -1,8 +1,13 @@
 package edu.android.examplemap3;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,36 +16,51 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    LocationCallback callback;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        Intent intent = new Intent(this, GpsService.class);
+        startService(intent);
+        callback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Location location = locationResult.getLastLocation();
+                double lat = location.getLatitude();
+                double lon = location.getLongitude();
+
+                LatLng latLng = new LatLng(lat,lon);
+
+                updateGoogleMap(latLng);
+            }
+        };
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    private void updateGoogleMap(LatLng latLng) {
+        if(mMap ==null){
+            // GoogleMap 객체가 생성되어 있지 않을 때는 화면 업데이트 불가능
+            return;
+        }
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(latLng));
+//        mMap.setMinZoomPreference(15);
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+    
 }
